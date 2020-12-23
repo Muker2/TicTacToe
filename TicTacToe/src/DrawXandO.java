a15daa2a98c057f43f35705a295db92138b7bbf7
MarcRubenHadel <76179366+MarcRubenHadel@users.noreply.github.com>
2020-12-23 21:44:59 +0100

    Update DrawXandO.java

--- TicTacToe/src/DrawXandO.java
+++ TicTacToe/src/DrawXandO.java
@@ -1,14 +1,29 @@
 
 
-import java.awt.Color;
+import java.swing.JLabel;
 
 
 
-public class DrawXandO {
+public class DrawXandO extends JLabel{			//das die DrawXandO Klasse alle Eigenschaften des JLabels übernimmt
 
 	public static void main(String[] args) {
 		// TODO Auto-generated method stub
 
+		
+		
+		
+		
+		
+		
+		
+		
+		
+		
+		
+		
+	g.	
+		
+		
 	}
 
 }
